f706931cf3f84b769a3ce92921034849cfdb4b49
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-11-21 16:51:45 +0530

    MessageBox Display

--- app.py
+++ app.py
@@ -6,30 +6,38 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
+def convertTemp(kelvin):
+    return str(round(kelvin-273.15,2))+" °C"
+
 class App(Tk):
     def __init__(self):
         super().__init__()
         self.title("Weather App")
-        self.geometry('650x500')
-        self.minsize(500,500)
-        self.maxsize(1500,1500)
+        self.geometry('400x250')
+        self.maxsize(650,500)
         self.grid()
         self.create_widgets()
          
     def clearText(self):
         self.location_text.delete("1.0","end")
-        
-    def printMessage(self):
+         
+    def checkWeather(self):
         CITY = self.location_text.get("1.0",END).strip('\n')
         url = os.getenv('BASE_URL') + "appid=" + os.getenv("API_KEY") + "&q=" + CITY
         response = requests.get(url).json()
-        print(response['main']['temp'])
+        current_temp = response['main']['temp']
+        max_temp = response['main']['temp_max']
+        min_temp = response['main']['temp_min']
+        feels_like = response['main']['feels_like']
+        humidity = response['main']['humidity']
+        messagebox.showinfo(title=f"{CITY} Weather\n",message=f'WEATHER DETAILS\nTemperature: {convertTemp(current_temp)}\nMaximum Temperature: {convertTemp(max_temp)}\nMinimum Temperature: {convertTemp(min_temp)}\nFeels Like: {convertTemp(feels_like)}\nHumidity: {humidity}%')
     
     def create_widgets(self):
-        self.location_text = Text(self,bg='light cyan')
-        self.location_text.grid(row=0,column=0,columnspan=2,sticky='nsew')
-        self.button1 = Button(self,text='Clear',command=self.clearText,background='grey').grid(row=1,column=0,sticky='nsew')
-        self.button2 = Button(self,text='Submit',command=self.printMessage,background='grey').grid(row=1,column=1,sticky='nsew')
+        self.location_label = Label(self,text="Enter City Name: ",font=("Monocraft",10)).grid(row=0,column=0,pady=(40,5),padx=(60,0))
+        self.location_text = Text(self,bg='light cyan',width=30,height=5)
+        self.location_text.grid(row=1,column=0,columnspan=2,pady=(0,20),padx=(60,0),sticky="nsew")
+        self.button2 = Button(self,text='Submit',command=self.checkWeather,background='grey',width=15).grid(row=2,column=0,sticky="nsew",padx=(60,0))
+        self.button1 = Button(self,text='Clear',command=self.clearText,background='grey',width=5).grid(row=2,column=1,sticky="nsew",padx=(60,0))
 
 if __name__=='__main__':
     app = App()
