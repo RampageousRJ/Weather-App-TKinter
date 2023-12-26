d08d512376b93f9778018ca02413ac297b312eed
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-12-26 16:02:46 +0530

    Minor Changes

--- virt/Scripts/activate.bat
+++ virt/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Study\OneDrive - Manipal Academy of Higher Education\Coding\Mini-Projects\Weather-App-TKinter\virt
+set VIRTUAL_ENV=E:\Study\OneDrive - Manipal Academy of Higher Education\Coding\Mini-Projects\Small-Scale Projects\Weather-App-TKinter\virt
 
 if not defined PROMPT set PROMPT=$P$G
 
